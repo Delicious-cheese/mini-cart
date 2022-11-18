1e14329b1f2acd0dac5aa898b5913fed5c2a3088
好喝的芝士 <llric_zhao@163.com>
2022-11-18 19:48:51 +0800

    fix style for navbar

--- src/components/MyNavBar.tsx
+++ src/components/MyNavBar.tsx
@@ -12,8 +12,8 @@ const MyNavBar = () => {
                 <Navbar.Brand href="/">
                     <img src={imgLogo} height="30" alt="" />
                 </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link to='/' as={NavLink}>主页</Nav.Link>
+                {/* <Nav className="me-auto"> 靠左 */}
+                <Nav>
                     <Nav.Link to='clothes' as={NavLink} >衣服</Nav.Link>
                     <Nav.Link to='/electronic' as={NavLink}>电器</Nav.Link>
                     <Nav.Link to='/gift' as={NavLink}>纪念品</Nav.Link>
